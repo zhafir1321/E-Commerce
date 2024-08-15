f8a23e3db857c013e53eb817ec6a1628e363f9f0
RizkyHidayat22 <rizkyhidayat0422@gmail.com>
2024-08-16 03:18:10 +0700

    added : handelAdmin

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,63 +1,99 @@
-const { Profile, User } = require('../models/index')
-const bcrypt = require('bcryptjs')
+const { USE } = require("sequelize/lib/index-hints");
+const { Profile, User } = require("../models/index");
+const bcrypt = require("bcryptjs");
+const { where } = require("sequelize");
 
 class Controller {
-
-    static async renderRegister(req, res) {
-        try {
-            res.render('register')
-        } catch (error) {
-            console.log(error);
-            res.send(error)
-        }
+  static async renderRegister(req, res) {
+    try {
+      res.render("register");
+    } catch (error) {
+      console.log(error);
+      res.send(error);
     }
+  }
 
-    static async handleRegister(req, res) {
-        try {
-            const {username, name, password, phoneNumber, email, role} = req.body
-            const user = await User.create({ username, password, role });
-            await user.createProfile({name, phoneNumber, email, UserId: user.id})
-            
-            res.redirect('/login')
-        } catch (error) {
-            console.log(error);
-            res.send(error)
-        }
+  static async handleRegister(req, res) {
+    try {
+      const { username, name, password, phoneNumber, email, role } = req.body;
+      const user = await User.create({ username, password, role });
+      await user.createProfile({ name, phoneNumber, email, UserId: user.id });
+
+      res.redirect("/login");
+    } catch (error) {
+      console.log(error);
+      res.send(error);
     }
+  }
 
-    static async renderLogin(req, res) {
-        try {
-            const {error} = req.query
-            res.render('login', {error})
-        } catch (error) {
-            console.log(error);
-            res.send(error)
-        }
+  static async renderLogin(req, res) {
+    try {
+      const { error } = req.query;
+      res.render("login", { error });
+    } catch (error) {
+      console.log(error);
+      res.send(error);
     }
+  }
 
-    static async handleLogin(req, res) {
-        try {
-            const { username, password } = req.body
-            const user = await User.findOne({ where: { username } })
-            if (user) {
-                const isValidPassword = await bcrypt.compare(password, user.password)
+  static async handleLogin(req, res) {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ where: { username } });
+      if (user) {
+        const isValidPassword = await bcrypt.compare(password, user.password);
 
-                if (isValidPassword) {
-                    res.redirect('/')
-                } else {
-                    const error = 'Incorrect Password'
-                    res.redirect(`/login?error=${error}`)
-                }
-            } else {
-                const error = 'Username not found!'
-                res.redirect(`/login?error=${error}`)
-            }
-        } catch (error) {
-            console.log(error);
-            res.send(error)
+        if (isValidPassword) {
+          res.redirect("/");
+        } else {
+          const error = "Incorrect Password";
+          res.redirect(`/login?error=${error}`);
         }
+      } else {
+        const error = "Username not found!";
+        res.redirect(`/login?error=${error}`);
+      }
+    } catch (error) {
+      console.log(error);
+      res.send(error);
     }
+  }
 
+  static async renderAdmin(req, res) {
+    try {
+      let data = await Profile.findAll({
+        include: User,
+      });
+      // res.send(data)
+      res.render("admin", { data });
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
+  }
+
+  static async handelDelete(req, res) {
+    try {
+      let { UserId } = req.params;
+
+      await Profile.destroy({
+        where: {
+          id: UserId,
+        },
+      });
+
+      await User.destroy({
+        where: {
+          id: UserId,
+        },
+      });
+
+      res.redirect("/admin");
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
+  }
 }
 
-module.exports = Controller
+module.exports = Controller;
