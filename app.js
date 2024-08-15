9f35b29a8ffe39505afc0b7c6044430fecaf1c5e
RizkyHidayat22 <rizkyhidayat0422@gmail.com>
2024-08-16 03:44:58 +0700

    added : AddAdminInrouter

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 
+
 app.get('/register', Controller.renderRegister)
 app.post('/register', Controller.handleRegister)
 app.get('/login', Controller.renderLogin)
 app.post('/login', Controller.handleLogin)
+app.get('/admin', Controller.renderAdmin)
+app.get('/admin/:UserId/delete', Controller.handelDelete)
 
 app.listen(3000, () => {
     console.log(`SERVER CONNECTED`);
